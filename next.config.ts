3d8edd1fed329f5bba0e1b67af4d240808f9415d
Aayushsoni009 <aayush@pgagi.in>
2026-05-29 16:56:07 +0530

    Update next.config.ts for Amplify SSR support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'standalone', // critical for Amplify SSR
+}
 
-export default nextConfig;
+export default nextConfig
